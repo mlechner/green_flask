5172b8a317b19cf55654b2c283d0fa2ceebb1880
Marco Lechner, in medias res GmbH <lechner@webgis.de>
2015-03-18 16:19:53 +0100

    no idea what changes have been done

--- index.py
+++ index.py
@@ -6,28 +6,33 @@ from sqlalchemy.orm import *
 import geojson
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from geoalchemy import *
-from geoalchemy.spatialite import SQLiteComparator
-from pysqlite2 import dbapi2 as sqlite
+#from geoalchemy import *
+#from geoalchemy.spatialite import SQLiteComparator
+#from pysqlite2 import dbapi2 as sqlite
+#import pysqlite2
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:////tmp/green_flask.db', module=sqlite, echo=True)
-connection = engine.raw_connection().connnection
-connection.enable_load_extension(True)
+#engine = create_engine('sqlite:////tmp/green_flask.db', module=sqlite, echo=True)
+engine = create_engine('sqlite:///green_flask.db')
+#connection = engine.raw_connection().connnection
+#connection.enable_load_extension(True)
 metadata = MetaData(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
-session.execute("select load_extension('/usr/local/lib/libspatialite.so')")
+#session.execute("select load_extension('/usr/local/lib/libspatialite.so')")
 Base = declarative_base(metadata=metadata)
 
-class Area(Base):
+class Geom(Base):
     __tablename__ = 'area'
     id = Column(Integer, primary_key=True)
     name = Column(Unicode, nullable=True)
-    geom = GeometryColumn(MultiPolygon(2))
+    #geom = GeometryColumn(MultiPolygon(2))
+    geom = Column(Text, nullable=True) 
     
-GeometryDDL(Area,__table__)
+#GeometryDDL(Area,__table__)
+
+geocoll = session.query(Geom).all()
 
 @app.route('/')
 def hello_world():
@@ -35,7 +40,7 @@ def hello_world():
 
 @app.route('/areas', methods=['GET'])
 def areas_get():
-    return geojson.dumps(polycoll)
+    return geojson.dumps(geocoll)
 
 @app.route('/area/get/<int:id>', methods=['GET'])
 def area_get(id):
@@ -45,12 +50,17 @@ def area_get(id):
 def area_add():
     r = request
     if request.method == 'POST':
-        mypoly = geojson.loads(r.data)
-        print mypoly
-        for feature in mypoly.features:
-            polycoll.features.append(feature)
-        polystr = geojson.dumps(polycoll)
-        return polystr
+        mygeo = geojson.loads(r.data)
+        if mygeo.type == 'FeatureCollection':
+            for feature in mygeo.features:
+                f = Geom()
+                f.id = feature.id
+                f.geom = geojson.dumps(feature.geometry)
+                geocoll.features.append(f)
+            geostr = geojson.dumps()
+            return geostr
+        else:
+            return 'type is not FeatureCollection but %s' %(mygeo.type)
     else:
         return 'New area added using GET.'
 
